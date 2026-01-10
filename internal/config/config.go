a9853067b23702c46766ebe7a1b481771effda3e
Grigorii Vakhnin <>
2026-01-10 17:09:56 +0300

    Правки

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,7 +8,7 @@ type Config struct {
 }
 
 func CreateConfig() *Config {
-	host := flag.String("a", "http://localhost:8080", "host. default value: localhost")
+	host := flag.String("a", "localhost:8080", "host. default value: localhost")
 	getURLHost := flag.String("b", "http://localhost:8080", "host in getURL response")
 	flag.Parse()
 
