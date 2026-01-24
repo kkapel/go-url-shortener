c6ee17e66bc6e66da9bbfada74e02b12a71bdbc8
Grigorii Vakhnin <>
2026-01-24 16:35:03 +0300

    iter5

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module go-url-shortener
 go 1.24.11
 
 require (
+	github.com/caarlos0/env v3.5.0+incompatible
+	github.com/caarlos0/env/v6 v6.10.1
 	github.com/go-chi/chi/v5 v5.2.3
 	github.com/stretchr/testify v1.11.1
 )
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,16 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"log"
+
+	"github.com/caarlos0/env/v6"
+)
+
+type ConfigVariable struct {
+	serverAddress string `env:"SERVER_ADDRESS"`
+	baseURL       string `env:"BASE_URL"`
+}
 
 type Config struct {
 	Host       string
@@ -8,12 +18,41 @@ type Config struct {
 }
 
 func CreateConfig() *Config {
-	host := flag.String("a", "localhost:8080", "host. default value: localhost")
-	getURLHost := flag.String("b", "http://localhost:8080", "host in getURL response")
+	//Если указана переменная окружения, то используется она.
+	var configVariable ConfigVariable
+	var resultHost, resultGetURLHost string
+
+	err := env.Parse(&configVariable)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	varHost := configVariable.serverAddress
+	varGetURLHost := configVariable.baseURL
+
+	//Если нет переменной окружения, но есть аргумент командной строки (флаг), то используется он.
+	flagHost := flag.String("a", "", "host. default value: localhost")
+	flagGetURLHost := flag.String("b", "", "host in getURL response")
 	flag.Parse()
 
+	if varHost != "" {
+		resultHost = varHost
+	} else if *flagHost != "" {
+		resultHost = *flagHost
+	} else {
+		resultHost = "localhost:8080"
+	}
+
+	if varGetURLHost != "" {
+		resultGetURLHost = varGetURLHost
+	} else if *flagGetURLHost != "" {
+		resultGetURLHost = *flagGetURLHost
+	} else {
+		resultGetURLHost = "http://localhost:8080"
+	}
+
 	return &Config{
-		Host:       *host,
-		GetURLHost: *getURLHost,
+		Host:       resultHost,
+		GetURLHost: resultGetURLHost,
 	}
 }
