73f0a50a55a2709ed4112c64f4361eaf0e1385c3
Grigorii Vakhnin <>
2026-02-06 12:02:56 +0300

    Добавлено логирование

--- internal/router/init_server.go
+++ internal/router/init_server.go
@@ -3,6 +3,7 @@ package router
 import (
 	"go-url-shortener/internal/config"
 	"go-url-shortener/internal/handler"
+	"go-url-shortener/internal/loger"
 	"go-url-shortener/internal/repository"
 	"net/http"
 
@@ -12,6 +13,10 @@ import (
 func Run() error {
 	repo := repository.NewURLRepository()
 	cfg := config.CreateConfig()
+	if err := loger.Initialize("INFO"); err != nil {
+		return err
+	}
+	defer loger.Log.Sync()
 
 	h := &handler.Handler{
 		Repo: repo,
@@ -19,6 +24,7 @@ func Run() error {
 	}
 
 	r := chi.NewRouter()
+	r.Use(loger.RequestLogger)
 	r.Post("/", h.APIPagePost)
 	r.Get("/{id}", h.APIPageGet)
 
