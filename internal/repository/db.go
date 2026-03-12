b49de1b03d77c2fd17bf11384ce1a69bc3d18f60
Grigorii Vakhnin <>
2026-03-12 11:43:10 +0300

    iter15: Обновление таблиц бд

--- internal/repository/db.go
+++ internal/repository/db.go
@@ -61,13 +61,15 @@ func InitDB(dbConnect string) error {
 		short_link VARCHAR(100) UNIQUE,
 		long_link VARCHAR(500) UNIQUE,
 		user_id int,
-		deleted_flag boolean
+		deleted_flag boolean,
+		change_time timestamp
 		);
 
 		comment on column short_url.id is 'ID записи';
 		comment on column short_url.short_link is 'Короткий URL';
 		comment on column short_url.long_link is 'Длинный URL';
 		comment on column short_url.deleted_flag is 'Флаг удаленного сокращенного URL';
+		comment on column short_url.change_time is 'Время занесения/изменения значения';
 		-- Создание таблицы users_token
 		create table if not exists users_token
 		(
--- migrations/000001_create_short_url_table.up.sql
+++ migrations/000001_create_short_url_table.up.sql
@@ -5,10 +5,12 @@ id serial primary key,
 short_link VARCHAR(100) UNIQUE,
 long_link VARCHAR(500) UNIQUE,
 user_id int,
-deleted_flag boolean
+deleted_flag boolean,
+change_time timestamp
 );
 
 comment on column short_url.id is 'ID записи';
 comment on column short_url.short_link is 'Короткий URL';
 comment on column short_url.long_link is 'Длинный URL';
 comment on column short_url.deleted_flag is 'Флаг удаленного сокращенного URL';
+comment on column short_url.change_time is 'Время занесения/изменения значения';
