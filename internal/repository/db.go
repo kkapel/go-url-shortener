286090ce1d9d22b8ae9f5fab8d669784f3738018
Grigorii Vakhnin <>
2026-03-16 16:40:34 +0300

    iter15: Правки по замечаниям

--- internal/repository/db.go
+++ internal/repository/db.go
@@ -51,37 +51,39 @@ func InitDB(dbConnect string) (*DB, error) {
 		return nil, err
 	}
 
-	query :=
-		`create table IF NOT EXISTS short_url
-		(
-		id serial primary key,
-		short_link VARCHAR(100) UNIQUE,
-		long_link VARCHAR(500) UNIQUE,
-		user_id int,
-		deleted_flag boolean,
-		change_time timestamp
-		);
-
-		comment on column short_url.id is 'ID записи';
-		comment on column short_url.short_link is 'Короткий URL';
-		comment on column short_url.long_link is 'Длинный URL';
-		comment on column short_url.deleted_flag is 'Флаг удаленного сокращенного URL';
-		comment on column short_url.change_time is 'Время занесения/изменения значения';
-		-- Создание таблицы users_token
-		create table if not exists users_token
-		(
+	/*
+		query :=
+			`create table IF NOT EXISTS short_url
+			(
 			id serial primary key,
-			accessToken VARCHAR(100) UNIQUE
-		);
+			short_link VARCHAR(100) UNIQUE,
+			long_link VARCHAR(500) UNIQUE,
+			user_id int,
+			deleted_flag boolean,
+			change_time timestamp
+			);
 
-		comment on column users_token.id is 'ID пользователя';
-		comment on column users_token.accessToken is 'Токен пользователя';`
+			comment on column short_url.id is 'ID записи';
+			comment on column short_url.short_link is 'Короткий URL';
+			comment on column short_url.long_link is 'Длинный URL';
+			comment on column short_url.deleted_flag is 'Флаг удаленного сокращенного URL';
+			comment on column short_url.change_time is 'Время занесения/изменения значения';
+			-- Создание таблицы users_token
+			create table if not exists users_token
+			(
+				id serial primary key,
+				accessToken VARCHAR(100) UNIQUE
+			);
 
-	_, err = db.Exec(query)
+			comment on column users_token.id is 'ID пользователя';
+			comment on column users_token.accessToken is 'Токен пользователя';`
 
-	if err != nil {
-		return nil, err
-	}
+		_, err = db.Exec(query)
+
+		if err != nil {
+			return nil, err
+		}
+	*/
 
 	return databaseInstance, nil
 }
