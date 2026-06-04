46c2f0373f3084c6fec054adb7786c625ccaa07a
Grigorii Vakhnin <>
2026-06-04 19:04:21 +0300

    iter23: Использование ldflags

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"go-url-shortener/internal/router"
 	"log"
 )
 
+var (
+	buildVersion string
+	buildDate    string
+	buildCommit  string
+)
+
 func main() {
+	// stdout для отображения информации о сборке при запуске приложения
+	fmt.Println("Build version:", valueOrNA(buildVersion))
+	fmt.Println("Build date:", valueOrNA(buildDate))
+	fmt.Println("Build commit:", valueOrNA(buildCommit))
+
 	if err := router.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func valueOrNA(v string) string {
+	if v == "" {
+		return "N/A"
+	}
+	return v
 }
