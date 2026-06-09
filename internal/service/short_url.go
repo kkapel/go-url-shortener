01da4a37fa503136b364f684154d030b4a222cc7
Grigorii Vakhnin <>
2026-06-09 12:34:41 +0300

    Правки по замечаниям

--- internal/service/short_url.go
+++ internal/service/short_url.go
@@ -154,50 +154,6 @@ func (s *ShortenerService) batchWorkerDelete(ctx context.Context, data []string,
 	if err != nil {
 		loger.Log.Error("final batch delete error", zap.Error(err), zap.Int("user_id", userID))
 	}
-
-	/*
-		ticker := time.NewTicker(300 * time.Millisecond)
-		defer ticker.Stop() // Важно остановить таймер при выходе
-
-		var ids []string
-
-		for {
-			select {
-			case id, ok := <-inputCh:
-				if !ok {
-					// Если канал закрыт, удаляем то, что успели собрать
-					if len(ids) > 0 {
-						if err := s.dbRepo.SetDeletedFlag(ctx, ids, userID); err != nil {
-							loger.Log.Error("final batch delete error", zap.Error(err), zap.Int("user_id", userID))
-						}
-					}
-					return
-				}
-
-				ids = append(ids, id)
-				if len(ids) >= 100 {
-					if err := s.dbRepo.SetDeletedFlag(ctx, ids, userID); err != nil {
-						loger.Log.Error("batch delete error (limit reached)", zap.Error(err), zap.Int("user_id", userID))
-					}
-					ids = ids[:0]
-				}
-
-			case <-ticker.C:
-				if len(ids) > 0 {
-					if err := s.dbRepo.SetDeletedFlag(ctx, ids, userID); err != nil {
-						loger.Log.Error("batch delete error (ticker)", zap.Error(err), zap.Int("user_id", userID))
-					}
-					ids = ids[:0]
-				}
-
-			case <-ctx.Done():
-				// Если контекст отменили
-				loger.Log.Info("worker stopped by context")
-				return
-			}
-		}
-	*/
-
 }
 
 // fanOut принимает канал данных, порождает 10 горутин
